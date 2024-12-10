44c7183c9bcf7a68c24d3b5c57f3f2263c606a85
Krish Teckchandani <krish2005tech@gmail.com>
2024-12-10 14:57:38 +0530

    loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LandingPage from "./components/countdown/LandingPage.jsx";
+import timer from "../public/timer2.gif";
 
 import { useState, useEffect } from "react";
 
@@ -9,22 +10,29 @@ import Header from "./components/Header";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [isTransitioning, setIsTransitioning] = useState(false);
+  const [showFirstText, setShowFirstText] = useState(true);
 
   useEffect(() => {
     // Show the loader for 5 seconds
     const loadingTimer = setTimeout(() => {
       setIsLoading(false);
       setIsTransitioning(true); // Start the ripple transition
-    }, 5000);
+    }, 7000);
 
     // End the ripple transition after 2 seconds
     const transitionTimer = setTimeout(() => {
       setIsTransitioning(false);
-    }, 7000);
+    }, 8000);
+
+    // Toggle the loading text
+    const textTimer = setTimeout(() => {
+      setShowFirstText(false);
+    }, 5000);
 
     return () => {
       clearTimeout(loadingTimer);
       clearTimeout(transitionTimer);
+      clearTimeout(textTimer);
     };
   }, []);
 
@@ -32,15 +40,19 @@ function App() {
     <div className="App">
       {isLoading && (
         <div id="loader-screen">
-          <div className="loader"></div>
+          <div className="loader">
+            {showFirstText ? (
+              <p className='loading-text text1'>THE STAGE IS SET</p>
+            ) : (
+              <p className='loading-text text2'>ARE YOU READY ?</p>
+            )}
+            <img src={timer} alt="Loading..." />
+          </div>
         </div>
       )}
       {isTransitioning && <div id="ripple-mask"></div>}
       {!isLoading && !isTransitioning && (
         <div className="main-page">
-          {/* <Routes>
-            <Route path="/" element={<LandingPage />} />
-          </Routes> */}
           <LandingPage />
         </div>
       )}
